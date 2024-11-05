4b9dc7f1468c4d014c2ca5f9b8b78b3311e1a6e6
Johannes <me@johannes-bln.de>
2024-11-05 22:13:25 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["DateExtension"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/johannesxdev/DateExtension.git", from: "1.0.0")
+        .package(url: "https://github.com/johannes-bln/DateExtension.git", from: "1.0.0")
     ], targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
